af0c366a1591f1adc2351e846c6dc4ccaf441aa6
David Stinghen <davidstinghen@gmail.com>
2019-10-03 01:21:37 -0300

    feature: create methods to store bookings

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require("./config/upload");
 const SessionController = require("./app/controllers/SessionController");
 const SpotController = require("./app/controllers/SpotController");
 const DashboardController = require("./app/controllers/DashboardController");
+const BookingsController = require("./app/controllers/BookingController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -15,5 +16,7 @@ routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 routes.get("/spots", SpotController.index);
 
 routes.get("/dashboard", DashboardController.show);
+
+routes.post("/spots/:id/bookings", BookingsController.store);
 
 module.exports = routes;
